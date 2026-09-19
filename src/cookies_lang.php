125d8aa6a97b45c1f4f78eefea068d580838f437
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-09-19 19:53:01 +0100

    Initial Commit!

---
+++ src/cookies_lang.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Cookies language file.
+ *
+ * Contains labels and messages used by the cookie-consent banner,
+ * cookie settings UI, and cookie policy helpers.
+ */
+
+$lang['cookies_consent_title'] = 'Folosim cookie-uri';
+$lang['cookies_consent_message'] = 'Folosim cookie-uri necesare pentru ca acest site să funcționeze. Cu permisiunea dvs., folosim și cookie-uri de analiză și marketing pentru a înțelege cum folosesc vizitatorii site-ul și pentru a ne îmbunătăți conținutul.';
+$lang['cookies_consent_accept_all'] = 'Acceptați toate';
+$lang['cookies_consent_accept_required'] = 'Doar necesare';
+
+/**
+ * ---------------------------------------------------------------
+ * Cookie Categories
+ * ---------------------------------------------------------------
+ */
+
+$lang['cookies_category_necessary_title'] = 'Cookie-uri necesare';
+$lang['cookies_category_necessary_label'] = 'Întotdeauna active';
+$lang['cookies_category_necessary_description'] = 'Aceste cookie-uri sunt necesare pentru ca site-ul să funcționeze corect. Ele susțin funcții de bază, cum ar fi consimțământul pentru cookie-uri, preferințele de limbă, autentificarea, securitatea, sesiunile și verificarea dispozitivelor de încredere. Sunt întotdeauna active deoarece sunt necesare pentru funcționarea site-ului.';
+
+$lang['cookies_category_analytics_title'] = 'Cookie-uri de analiză';
+$lang['cookies_category_analytics_label'] = 'Opționale';
+$lang['cookies_category_analytics_description'] = 'Aceste cookie-uri ne ajută să înțelegem cum folosesc vizitatorii site-ul, cum ar fi ce pagini sunt vizitate și cum interacționează utilizatorii cu site-ul. Aceste informații ne ajută să îmbunătățim performanța, utilizarea și conținutul. Cookie-urile de analiză sunt folosite doar atunci când sunt activate de administratorul site-ului și acceptate de vizitator.';
+
+$lang['cookies_category_marketing_title'] = 'Cookie-uri de marketing';
+$lang['cookies_category_marketing_label'] = 'Opționale';
+$lang['cookies_category_marketing_description'] = 'Aceste cookie-uri pot fi folosite pentru a măsura campanii, a personaliza publicitatea sau a integra servicii de marketing, cum ar fi pixelii publicitari. Ele pot fi setate de servicii terțe configurate de administratorul site-ului și sunt folosite doar atunci când sunt acceptate de vizitator.';
